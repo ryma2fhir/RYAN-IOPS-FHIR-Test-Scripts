cf40f89397ed7a41595c197dcbe0a1365b721ed7
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2021-11-04 17:41:37 +0000

    Fixed onto url assignment issue

--- src/configureValidator.ts
+++ src/configureValidator.ts
@@ -11,13 +11,14 @@ const args = require('minimist')(process.argv.slice(2))
 
 const destinationPath = '../../validation-service-fhir-r4/src/main/resources';
 
-var ontoServer = 'https://ontology.nhs.uk/authoring/fhir/'
 
-if (process.env.ONTO_CLIENT_ID!= undefined) {
-    ontoServer = process.env.ONTO_CLIENT_ID;
+var ontoServer: string = 'https://ontology.nhs.uk/authoring/fhir/'
+if (process.env.ONTO_URL!= undefined) {
+    ontoServer = process.env.ONTO_URL;
 }
 var clientId: string = process.env.ONTO_CLIENT_ID
 var clientSecret: string = process.env.ONTO_CLIENT_SECRET
+
 
 class TarMe {
     static async main(src, destination) {
