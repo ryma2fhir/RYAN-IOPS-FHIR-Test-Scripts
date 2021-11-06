fd3a057df48b39ac67b3937f2590b0ea6b5ef116
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2021-11-06 08:45:45 +0000

    Validator tests

--- src/validator.test.ts
+++ src/validator.test.ts
@@ -16,7 +16,7 @@ it('Validator is functioning ',async function () {
 function testFile(file) {
     const resource: any = fs.readFileSync(file, 'utf8');
 
-    it('Validate ' + file, async () => {
+    it('Test HL7 FHIR resource passes validation ' + file, async () => {
         // Initial terminology queries can take a long time to process - cached responses are much more responsive
         jest.setTimeout(30000)
         await client()
@@ -34,10 +34,10 @@ function testFile(file) {
     });
 }
 
-function testFileError(file,message) {
+function testFileError(testDescription, file,message) {
     const resource: any = fs.readFileSync(file, 'utf8');
 
-    it('Validate ' + file, async () => {
+    it(testDescription + ' Filename = ' + file, async () => {
         // Initial terminology queries can take a long time to process - cached responses are much more responsive
         jest.setTimeout(30000)
         await client()
@@ -66,15 +66,16 @@ describe('Testing validation api is functioning', () => {
 });
 
 describe('Testing validation fails simple resource', () => {
-    testFileError('Examples/fail/patientError.json','Patient.identifier:nhsNumber: minimum required = 1')
+    testFileError('Check validation fails when no NHS Number is supplied','Examples/fail/patientError.json','Patient.identifier:nhsNumber: minimum required = 1')
 
-    testFileError('Examples/fail/MedicationRequest-missingMedication.json',undefined)
-    testFileError('Examples/fail/MedicationRequest-missingMedication.json','MedicationRequest.medication[x]: minimum required = 1')
-    testFileError('Examples/fail/MedicationRequest-dmdCode.json','CodeableConcept.coding:SNOMED: minimum required = 1')
+    testFileError('Check validation fails when no medication code is supplied', 'Examples/fail/MedicationRequest-missingMedication.json',undefined)
+    testFileError('Check validation fails when no medication code is supplied','Examples/fail/MedicationRequest-missingMedication.json','MedicationRequest.medication[x]: minimum required = 1')
+    testFileError('Check validation fails when medication code is supplied with the future dm+d system', 'Examples/fail/MedicationRequest-dmdCode.json','CodeableConcept.coding:SNOMED: minimum required = 1')
 
-    testFileError('Examples/fail/MedicationDispense-invalidaUnitOfMeasure.json','Validation failed for \'http://unitsofmeasure.org')
-    testFileError('Examples/fail/MedicationDispense-dmdCode.json','CodeableConcept.coding:SNOMED: minimum required = 1')
-
-    testFileError('Examples/fail/Bundle-prescripton-order-12 Item-incorrectFocus.json', undefined)
+    testFileError('Check validation fails when daysSupply has an incorrect unitsofmeasure code','Examples/fail/MedicationDispense-invalidaUnitOfMeasure.json','Validation failed for \'http://unitsofmeasure.org')
+    testFileError('Check validation fails when medication code is supplied with the future dm+d system','Examples/fail/MedicationDispense-dmdCode.json','CodeableConcept.coding:SNOMED: minimum required = 1')
+    testFileError('Check validation fails when identifier is an object not an array','Examples/fail/MedicationRequest-invalidJSON.json', undefined)
+    testFileError('Check validation fails when MedicationRequest is not referenced in the MessageHeader.focus but is present','Examples/fail/Bundle-prescripton-order-12 Item-incorrectFocus.json', undefined)
+    testFileError('Check validation fails when Location is referenced but not present in the FHIR Message','Examples/fail/Bundle-prescripton-order-12 Item-locationNotPresent.json', undefined)
 });
 
