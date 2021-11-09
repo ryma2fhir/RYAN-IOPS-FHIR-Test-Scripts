c9958246748fc3a69e38fdcf84dbe811c0b41db3
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2021-11-09 15:32:17 +0000

    Amended error text search

--- src/validator.test.ts
+++ src/validator.test.ts
@@ -95,7 +95,7 @@ describe('Testing validation fails invalid FHIR resources', () => {
 
     testFileError('Check validation fails when MedicationRequest references Patient in the MessageHeader.focus but is present','Examples/fail/Bundle-prescription-order-incorrectFocus.json', 'Invalid Resource target type.')
     // Should be in MessageDefinition??
-    testFileError('Check validation fails when Location is referenced but not present in the FHIR Message','Examples/fail/Bundle-prescription-order-locationNotPresent.json', 'Unable to find a match for profile')
+    testFileError('Check validation fails when Location is referenced but not present in the FHIR Message','Examples/fail/Bundle-prescription-order-locationNotPresent.json', 'Unable to find')
 
     testFileError('Check validation fails when Message Bundle.entry.fullUrl is absent','Examples/fail/Bundle-prescription-order-missingFullUrl.json','Bundle entry missing fullUrl')
     testFileError('Check validation fails when SearchSet Bundle.entry.fullUrl is absent (AEA-1828)','Examples/fail/Bundle-searchset-COVIDExemption-missingFullUrl.json','fullUrl')
