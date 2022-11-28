4731526bdd5ca1e0f7a3ba2f8ba9a7abf1def4e6
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2022-11-28 15:00:12 +0000

    Turned group name into a heading for clarity of display

--- test-reporter/test-reporter.ts
+++ test-reporter/test-reporter.ts
@@ -21,9 +21,9 @@ export default class TestReporter implements CustomReporter {
                     lastGroupName = group[0]
                     gitHubSummary +=  '#### '+ lastGroupName + ' '+NEW_LINE;
                 }
-                if (result.status == 'passed') gitHubSummary += ':heavy_check_mark:'
-                if (result.status == 'failed') gitHubSummary += ':x:'
-                gitHubSummary +=  "    " + result.title +NEW_LINE;
+                if (result.status == 'passed') gitHubSummary += '    :heavy_check_mark:'
+                if (result.status == 'failed') gitHubSummary += '    :x:'
+                gitHubSummary +=  " " + result.title +NEW_LINE;
                 for (let error of result.failureMessages) {
                     gitHubSummary += NEW_LINE + ' - ' + error + NEW_LINE+ NEW_LINE
                 }
