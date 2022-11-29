b8f48ee37bb42d2af801571513ce5aa16f00d88a
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2022-11-29 05:47:51 +0000

    Git Sumary Tests

--- test-reporter/test-reporter.ts
+++ test-reporter/test-reporter.ts
@@ -10,34 +10,43 @@ export default class TestReporter implements CustomReporter {
 
     onRunComplete(_: Set<TestContext>, results: AggregatedResult) {
         let gitHubSummary = NEW_LINE + NEW_LINE+'### :fire: Report '+NEW_LINE;
-        const gitrepoBranch = process.env.GITHUB_REF_NAME
+        let gitrepoBranch = process.env.GITHUB_REF_NAME
         const gitrepoName = process.env.GITHUB_REPOSITORY
         if (gitrepoBranch != null) {
             gitHubSummary += ' Branch '+ gitrepoBranch+' '+NEW_LINE;
+        } else {
+            gitrepoBranch = 'main';
         }
         if (gitrepoName != null) {
             gitHubSummary += ' Name '+ gitrepoName+' '+NEW_LINE+NEW_LINE;
         }
         gitHubSummary += ' :x: Failed '+ results.numFailedTests+' '+NEW_LINE;
-        gitHubSummary += ' :heavy_check_mark: Passed '+ results.numPassedTests+' '+NEW_LINE;
+        gitHubSummary += ' :white_check_mark: Passed '+ results.numPassedTests+' '+NEW_LINE;
         gitHubSummary += NEW_LINE+NEW_LINE;
         for(let parent of results.testResults) {
             let lastGroupName='';
-            for(let result of parent.testResults) {
-                let group = result.fullName.split(result.title)
-                if (lastGroupName == '' || (group.length>0 && lastGroupName != group[0])) {
-                    lastGroupName = group[0]
-                    if (lastGroupName.includes('.') && gitrepoBranch != undefined) {
-                        gitHubSummary += '[' + lastGroupName.replace(" ", "/") + '](../../blob/'+gitrepoBranch+'/' + lastGroupName.replace(" ", "/") + ') ' + NEW_LINE;
-                    } else {
-                        gitHubSummary +=  '#### '+ lastGroupName + ' '+NEW_LINE;
+            for (let status of ['failed','passed']) {
+                if (status == 'failed') gitHubSummary += '### Fail'+NEW_LINE+NEW_LINE;
+                if (status == 'passed') gitHubSummary += '### Pass'+NEW_LINE+NEW_LINE;
+                for (let result of parent.testResults) {
+                    if (result.status == status) {
+                        let group = result.fullName.split(result.title)
+                        if (lastGroupName == '' || (group.length > 0 && lastGroupName != group[0])) {
+                            lastGroupName = group[0]
+                            if (lastGroupName.includes('.') && gitrepoBranch != undefined) {
+                                gitHubSummary += '[' + lastGroupName.replace(" ", "/") + '](../../blob/' + gitrepoBranch + '/' + lastGroupName.replace(" ", "/") + ') ' + NEW_LINE;
+                            } else {
+                                gitHubSummary += '#### ' + lastGroupName + ' ' + NEW_LINE;
+                            }
+                        }
+                        if (result.status == 'passed') gitHubSummary += ' * :white_check_mark:'
+                        if (result.status == 'failed') gitHubSummary += ' * :x:'
+                        gitHubSummary += " " + result.title + NEW_LINE;
+                        for (let error of result.failureMessages) {
+                            error = error.replace(NEW_LINE,NEW_LINE + ' > ')
+                            gitHubSummary += NEW_LINE + ' > ' + error + NEW_LINE + NEW_LINE
+                        }
                     }
-                }
-                if (result.status == 'passed') gitHubSummary += ' * :heavy_check_mark:'
-                if (result.status == 'failed') gitHubSummary += ' * :x:'
-                gitHubSummary +=  " " + result.title +NEW_LINE;
-                for (let error of result.failureMessages) {
-                    gitHubSummary += NEW_LINE + ' - ' + error + NEW_LINE+ NEW_LINE
                 }
             }
         }
