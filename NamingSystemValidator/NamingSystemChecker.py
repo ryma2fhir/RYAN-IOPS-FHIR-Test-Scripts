aeeac2f454c54158aac62621a9d313defcc5014d
Ryan May <112563297+ryma2fhir@users.noreply.github.com>
2023-06-28 14:03:34 +0100

    Update NamingSystemChecker.py

--- NamingSystemValidator/NamingSystemChecker.py
+++ NamingSystemValidator/NamingSystemChecker.py
@@ -6,6 +6,17 @@ for path in paths:
     files = os.listdir('./'+path)
     print(path)
     for file in files:
+        '''open file to find element values'''
+        tree = ET.parse("./"+path+"/"+file)
+        root = tree.getroot()
+        
+        '''do not check retired assets'''
+        try:
+            if root.findall('.//{*}'+str('status'))[0].get('value') == 'retired':
+                break
+        except:
+            print("active",root.findall('.//{*}'+str('status'))[0].get('value'))
+            pass
         
         '''Check files are in correct folder '''
         if path == 'structuredefinitions' and file.split('-')[0]!='Extension' and file.split('-')[0]!='UKCore':
@@ -14,12 +25,9 @@ for path in paths:
             print("The file '"+file+"' has either an incorrect prefix or in the wrong folder '"+path+"'.")  
         if path == 'codesystems' and file.split('-')[0]!='CodeSystem':
             print("The file '"+file+"' has either an incorrect prefix or in the wrong folder '"+path+"'.") 
-        
-        '''open file'''
-        tree = ET.parse("./"+path+"/"+file)
-        root = tree.getroot()
-        elements = {'ID':'id','url':'url','name':'name','title':'title'}
+
         '''check for missing elements'''
+        elements = {'ID':'id','url':'url','name':'name','title':'title'}
         for key,value in elements.items():
             try:
                 elements[key]=(root.findall('.//{*}'+str(value))[0].get('value'))
@@ -27,19 +35,20 @@ for path in paths:
                 print("\tThe element '"+key+"' is missing")
                 break
             
-                '''check elements naming convention are correct'''
+        '''check elements naming convention are correct'''
         fileName = file.replace('.xml','')
         warnings = []
         if path == 'codesystems' or path == 'valuesets':
             fileName = '-'.join(fileName.split('-')[1:])
         if not fileName == elements['ID']:
-            warnings.append("\t\tThe 'id' element: '"+elements['ID']+"' is incorrect")
+            warnings.append("\t\tThe 'id' element: "+elements['ID']+" is incorrect")
         if not fileName == elements['url'].split('/')[-1]:
-            warnings.append("\t\tThe 'url' element: '"+elements['url']+"' is incorrect")
+            if not elements['url'].startswith('http://hl7.org/fhir/5.0/'): #passes any R5 extensions
+                warnings.append("\t\tThe 'url' element: "+elements['url']+" is incorrect")
         if not ''.join(fileName.split('-')) == elements['name'].split('/')[-1]:
-            warnings.append("\t\tThe 'name' element: '"+elements['name']+"' is incorrect")
+            warnings.append("\t\tThe 'name' element: "+elements['name']+" is incorrect")
         if not fileName.replace('-','') == elements['title'].replace(' ',''):
-            warnings.append("\t\tThe 'title' element: '"+elements['title']+"' is incorrect")
+            warnings.append("\t\tThe 'title' element: "+elements['title']+" is incorrect")
         if warnings:
             print("\t"+file)
             for x in warnings:
@@ -51,6 +60,4 @@ for examples in examplesPath:
     if not examples.endswith("Example.xml"):
         print("\t",examples," - The filename is does not have the suffix 'Example'")
               
-print("\n\nCheck Complete!")
-    
-        
+print("\n\nCheck Complete!")  
