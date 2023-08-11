be1a114cfacbbcfb653f422febef79e701b570b7
Ryan May <112563297+ryma2fhir@users.noreply.github.com>
2023-08-11 13:50:33 +0100

    Update QualityControlCheckerUKCore.py

--- QualityControlChecker/QualityControlCheckerUKCore.py
+++ QualityControlChecker/QualityControlCheckerUKCore.py
@@ -8,11 +8,7 @@ for path in paths:
     print(path)
     for file in files:
         '''open file to find element values'''
-        try:
-            tree = ET.parse("./"+path+"/"+file)
-        except:
-            print("\t",file,"- This file cannot be parsed. Ensure the xml code is correct")
-            continue
+        tree = ET.parse("./"+path+"/"+file)
         root = tree.getroot()
         
         '''do not check retired assets'''
