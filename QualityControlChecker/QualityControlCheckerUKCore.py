0d9a0220eeb52ddd715ed8fd2e293bf730dc9da8
Ryan May <112563297+ryma2fhir@users.noreply.github.com>
2023-08-25 12:12:07 +0100

    Update QualityControlCheckerUKCore.py

--- QualityControlChecker/QualityControlCheckerUKCore.py
+++ QualityControlChecker/QualityControlCheckerUKCore.py
@@ -90,8 +90,6 @@ for path in paths:
             warnings.append("\t\t"+elements['name']+" - The 'name' element is incorrect")
         if not fileName.replace('-','') == elements['title'].replace(' ',''):
             warnings.append("\t\t"+elements['title']+" - The 'title' element is incorrect")
-        if not elements['copyright'] == 'Copyright © 2021+ HL7 UK Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at  http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License. HL7® FHIR® standard Copyright © 2011+ HL7 The HL7® FHIR® standard is used under the FHIR license. You may obtain a copy of the FHIR license at  https://www.hl7.org/fhir/license.html.':
-            warnings.append("\t\tThe copyright is incorrect")
         if warnings:
             error=True
             print("\t",file)
@@ -142,7 +140,7 @@ for example in examplesPath:
         error=True
         print("\t",example,"The 'id' element is incorrect")
 
-'''Capabilitystatement Checker - checks if all s are in the CapabilityStatement'''
+'''CapabilityStatement Checker - checks if all Profiles are in the CapabilityStatement'''
 tree= ET.parse('./CapabilityStatement/CapabilityStatement-UKCore.xml')
 root = tree.getroot()
 
