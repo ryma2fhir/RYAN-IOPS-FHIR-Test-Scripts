2c7996f8943a5728f1d10b4cc4d7c02c9e2321a5
Adam Page <115618858+APageNHS@users.noreply.github.com>
2023-12-05 14:16:28 +0000

    Update QualityControlChecker.py - ODS url
    ORD url changed to https://digital.nhs.uk/services/organisation-data-service

--- QualityControlChecker/QualityControlChecker.py
+++ QualityControlChecker/QualityControlChecker.py
@@ -19,7 +19,7 @@ parentDir = os.path.basename(parent)
 
 '''Creates main variables for use with UKCore and NHSE assets'''
 ukcoreVar = {'project':'UKCore','urlPrefix': 'https://fhir.hl7.org.uk/','ignoreURLPrefix':['http://hl7.org/fhir/5.0/'],'org':'HL7 UK', 'email':'ukcore@hl7.org.uk'}
-nhseVar = {'project':'England','urlPrefix': 'https://fhir.nhs.uk/England/', 'ignoreURLPrefix':['http://hl7.org/fhir/5.0/','https://ord.nhs.uk/'],'org':'NHS England', 'email':'interoperabilityteam@nhs.net'}
+nhseVar = {'project':'England','urlPrefix': 'https://fhir.nhs.uk/England/', 'ignoreURLPrefix':['http://hl7.org/fhir/5.0/','https://digital.nhs.uk/services/organisation-data-service/'],'org':'NHS England', 'email':'interoperabilityteam@nhs.net'}
 mainVar = {}
 if 'ukcore' in parentDir.lower():
     mainVar = ukcoreVar
