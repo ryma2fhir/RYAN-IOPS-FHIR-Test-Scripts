870025470e30d6c66142b75452f996f8667306a9
Adam Page <115618858+APageNHS@users.noreply.github.com>
2024-01-05 10:03:42 +0000

    Update common.js.ts

--- src/common.js.ts
+++ src/common.js.ts
@@ -297,9 +297,7 @@ function raiseWarning(issue: OperationOutcomeIssue, failOnWarning:boolean): bool
  
     // COMMENT WAS: TODO this needs to be turned to true 1/8/2022 Warnings not acceptable on NHS Digital resources
     // Actual comment is: if error not handled above, return error if FailOnWarning is true 
-    if (failOnWarning == false) {
-        process.env.SKIPPEDWARNINGS +=1;
-    }
+    process.env.SKIPPEDWARNINGS +=1;
     return failOnWarning;
 }
 function raiseError(issue: OperationOutcomeIssue) : boolean {
