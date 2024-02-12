0604167981c8a08afbc929d89eb58fd387842ae5
Ryan May <ryan.may2@nhs.net>
2024-02-12 09:40:24 +0000

    attempt to hide duplicate msgs

--- src/common.js.ts
+++ src/common.js.ts
@@ -443,10 +443,10 @@ export function getStrictValidation() {
 
 const optionsFilePath = '../options.json';
 const { strictValidation, ErrorIfMetaProfilePresent, ignoreFolders, ignoreFiles } = setOptions(optionsFilePath);
-console.log('Strict Validation:', strictValidation);
-console.log('Hide Profile Check:', ErrorIfMetaProfilePresent);
-console.log('Ignore Folders:', ignoreFolders);
-console.log('Ignore Files:', ignoreFiles);
+// console.log('Strict Validation:', strictValidation);
+// console.log('Hide Profile Check:', ErrorIfMetaProfilePresent);
+// console.log('Ignore Folders:', ignoreFolders);
+// console.log('Ignore Files:', ignoreFiles);
 
 
 // Ignores folders from options.json within the FHIR repo and hardcoded foldernames within this function
